96e7d9bd8f6b60478a3ae863ac6bdace8cef36ce
Majid <majidh2008@yahoo.com>
2024-10-11 16:28:58 +0330

    adding about_us, blog, contact_us, tutorials to views and urls

--- DanakAcademy/shop/urls.py
+++ DanakAcademy/shop/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about_us/', views.about, name='about'),
+    path('tutorials/', views.tutorials, name='tutorials'),
+    path('blog/', views.blog, name='blog'),
+    path('contact_us/', views.contact_us, name='contact_us'),
 ]
--- DanakAcademy/shop/views.py
+++ DanakAcademy/shop/views.py
@@ -2,3 +2,19 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about_us.html')
+
+
+def tutorials(request):
+    return render(request, 'tutorials.html')
+
+
+def blog(request):
+    return render(request, 'blog.html')
+
+
+def contact_us(request):
+    return render(request, 'contact_us.html')
